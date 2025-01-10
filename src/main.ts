95fa7d41572804dbddef8fd14f8eac9712ee31e6
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-01-10 23:19:52 +0900

    prisma 설정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
   setupSwagger(app); //Swagger 문서를 생성
   await app.listen(3000); //포트 3000에서 HTTP 요청을 수신하도록 설정
 }
