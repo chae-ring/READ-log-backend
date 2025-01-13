e534f05e404e81b6d9f8f9bb5ae4779f34b7d634
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-01-13 13:56:18 +0900

    readings 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma.module';
 import { UsersModule } from './users/users.module';
+import { ReadingsModule } from './readings/readings.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     UsersModule,
+    ReadingsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
