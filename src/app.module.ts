f157413c0b2fa66a9beee4c655d7b478c070b137
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-01-14 01:05:33 +0900

    reviews 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { PrismaModule } from './prisma.module';
 import { UsersModule } from './users/users.module';
 import { ReadingsModule } from './readings/readings.module';
 import { AuthModule } from './auth/auth.module';
+import { ReviewsModule } from './reviews/reviews.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { AuthModule } from './auth/auth.module';
     UsersModule,
     ReadingsModule,
     AuthModule,
+    ReviewsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
