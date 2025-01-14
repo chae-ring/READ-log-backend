587d8af60fbe62561d7cd08308e57fd4634b3e65
chae-ring <149919388+chae-ring@users.noreply.github.com>
2025-01-14 14:55:20 +0900

    접근 설정

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,19 @@ import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS 설정 추가: 모든 도메인 및 모든 메서드 허용
+  app.enableCors({
+    origin: '*', // 모든 도메인 허용
+    methods: '*', // 모든 HTTP 메서드 허용
+    allowedHeaders: 'Content-Type, Authorization', // 허용할 헤더
+  });
+
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
-  setupSwagger(app); //Swagger 문서를 생성
-  await app.listen(3000); //포트 3000에서 HTTP 요청을 수신하도록 설정
+
+  setupSwagger(app); // Swagger 문서를 생성
+
+  await app.listen(3000); // 포트 3000에서 HTTP 요청을 수신하도록 설정
 }
 bootstrap();
